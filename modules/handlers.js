7174e3467743278bee27e68462a23d4f36907e7a
Ilona Pastucha <ilonka.p4@gmail.com>
2018-09-11 13:12:12 +0200

    Finish task

---
+++ modules/handlers.js
@@ -0,0 +1,67 @@
+var fs = require('fs');
+var formidable = require('formidable');
+
+
+exports.upload = function(request, response) {
+  console.log("Rozpoczynam obsługę żadania upload");
+  var form = new formidable.IncomingForm();
+  form.parse(request, function(err, fields, files) {
+    if (err) { console.error(err); }
+
+    var name = (fields.title || 'test') + '.png';
+    fs.writeFile('./imageData.txt', name);
+    fs.renameSync(files.upload.path, name);
+    response.writeHead(200, {"Content-Type": "text/html"});
+    fs.readFile('templates/upload.html', function(err, html) {
+      if (err) {
+        response.writeHead(404);
+        response.write("404 :(");
+      } else {
+        response.write(html);
+        response.write("<img src='/show'/>");
+        response.end();
+      }
+    });
+  });
+};
+
+exports.uploadCSS = function(request, response) {
+  fs.readFile('css/upload.css', function(err, css) {
+    if (err) { console.error(err); }
+
+    response.writeHead(200, {"Content-Type": "text/css"});
+    response.write(css);
+    response.end();
+  });
+};
+
+exports.welcome = function(request, response) {
+  console.log("Rozpoczynam obsługę żądania welcome");
+  fs.readFile('templates/start.html', function(err, html) {
+    if (err) { console.error(err); }
+
+    response.writeHead(200, {"Content-Type": "text/html; charset=utf-8"});
+    response.write(html);
+    response.end();
+  });
+};
+
+exports.show = function(request, response) {
+  fs.readFile('imageData.txt', 'binary', function(err, file) {
+    if (err) { console.error(err); }
+
+    fs.readFile(file, 'binary', function(err, data) {
+      if (err) { console.error(err); }
+
+      response.writeHead(200, {"Content-Type": "image/png"});
+      response.write(data, 'binary');
+      response.end();
+    });
+  });
+};
+
+exports.error = function(request, response) {
+  console.log("Nie wiem co robić.");
+  response.write("404 :(");
+  response.end();
+};
